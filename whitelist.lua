8ebd8b8829d8fa789eb7b17328340a78aea28140
15travisss <stoptying3@gmail.com>
2024-08-04 02:31:02 -0500

    Update file with user ID 23423423423432

--- whitelist.lua
+++ whitelist.lua
@@ -1,3 +1,3 @@
-VIPWhitelist = {   }
-MVPWhitelist = { 234324234234, 234234 }
-EliteWhitelist = {   }
+VIPWhitelist = {     }
+MVPWhitelist = {  234324234234, 234234  }
+EliteWhitelist = { 23423423423432 }
